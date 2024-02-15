1a95940c3b896582fb4c61f9b479f7d399d5fb2e
HomedelMetall <c.escorihuela.sayalero@gmail.com>
2024-02-15 01:11:27 +0100

    0.0.1

--- src/ctoolkit/global_vars/version.py
+++ src/ctoolkit/global_vars/version.py
@@ -1,8 +1,8 @@
 def print_version():
 	print("Toolkit v_%s"%tk_version)
 
-tk_version = '1.6alpha_RustyRocker'
-tk_date = '2023-11-20 @ 12:46:32'
+tk_version = '0.0.1'
+tk_date = '2024-02-15 @ 01:10:37'
 if __name__=='__main__':
 	print('Version:', tk_version)
 	print('Date:', tk_date)
